a2abdb4e1e4a19b4b83e7553895e569bbc740289
Joshua Moore <40312633+joshuamoorexyz@users.noreply.github.com>
2023-08-09 13:03:17 -0400

    edits

--- srsrandeploy.sh
+++ srsrandeploy.sh
@@ -1,3 +1,18 @@
+sudo apt-get install build-essential cmake libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev libtool autoconf
+sudo apt-get install libzmq3-dev
+sudo add-apt-repository ppa:ettusresearch/uhd
+sudo apt-get update
+sudo apt-get install libuhd-dev libuhd4.1.0 uhd-host
+sudo apt install libtool autoconf
+git clone https://gitlab.eurecom.fr/oai/asn1c.git
+cd asn1c
+git checkout velichkov_s1ap_plus_option_group
+autoreconf -iv
+./configure
+make -j`nproc`
+sudo make install
+sudo ldconfig
+cd ..
 cd srsRAN-e2
 mkdir build
 export SRS=`realpath .`
